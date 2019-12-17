f1cf8cf68c1789e39da433c4d707d6d4ac757e10
Tiger <573556382@qq.com>
2019-12-17 15:16:25 +0800

    fix:composer

---
+++ BeanFactory.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * ====================================
+ * BeanFactory.php
+ * ====================================
+ * Author: Tiger
+ * Date: 2019/12/17
+ * ====================================
+ */
+
+namespace Core;
+
+
+class BeanFactory
+{
+
+    public function getHelloWorld()
+    {
+        return 'HelloWorld...';
+    }
+
+}
---
+++ annotations/Bean.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * ====================================
+ * Bean.php
+ * ====================================
+ * Author: Tiger
+ * Date: 2019/12/17
+ * ====================================
+ */
+
+namespace Core\Annotations;
+
+
+class Bean
+{
+
+    public function getTest()
+    {
+        return 'Test...';
+    }
+
+}
